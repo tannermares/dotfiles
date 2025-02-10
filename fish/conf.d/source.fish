fc50eb4092137ccf9381b8a5977ead10a3effc43
Tanner Mares <tannermares@gmail.com>
2025-02-10 07:28:21 -0800

    added sbin to fish path

--- fish/conf.d/source.fish
+++ fish/conf.d/source.fish
@@ -1,6 +1,7 @@
 if not set --query --global MY_SOURCE_SET
   set --global MY_SOURCE_SET true
   fish_add_path /home/linuxbrew/.linuxbrew/bin
+  fish_add_path /home/linuxbrew/.linuxbrew/sbin
 
   fzf --fish | source
   starship init fish | source
