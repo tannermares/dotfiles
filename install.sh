014bc26fdf85246f389fbbce15a6575e96efb3ea
Tanner Mares <tannermares@gmail.com>
2025-05-19 22:11:34 -0700

    swapped gimp for krita

--- install.sh
+++ install.sh
@@ -41,26 +41,26 @@ brew install bat btop cmake deno fastfetch ffmpeg fish fzf git-filter-repo go la
 
 # Install cask apps
 #  1. Discord           (Voice and text chat)    https://discord.com/
-#  2. Gimp              (GNU Image Manipulation) https://www.gimp.org/
 #  2. GPG Suite         (manage GPG keys)        https://gpgtools.org/
 #  3. Keeping You Awake (prevent system sleep)   https://keepingyouawake.app/
-#  4. LibreWolf         (Firefox clone)          https://librewolf.net/
-#  5. Logitech G HUB    (light controller)       https://www.logitechg.com/en-us/innovation/g-hub.html
-#  6. Medis             (Modern GUI for Redis)   https://getmedis.com/
-#  7. pgAdmin           (PostgresSQL admin)      https://www.pgadmin.org/
-#  8. PostgresApp       (Postgres App)           https://postgresapp.com/
-#  9. QuickLook Plugins (makes quicklook better) https://www.quicklookplugins.com/
-# 10. RapidAPI          (api client)             https://rapidapi.com
-# 11. Rectangle         (window manager)         https://rectangleapp.com/
-# 12. Rocket            (emoji app)              https://matthewpalmer.net/rocket/
-# 13. Sequel Ace        (database client)        https://sequel-ace.com/
-# 14. Slack             (chat client)            https://slack.com/
-# 15. SQLiteStudio      (sqlite database client) https://sqlitestudio.pl/
-# 16. StreamLabs        (streaming software)     https://streamlabs.com/
-# 17. VSCode            (text editor)            https://code.visualstudio.com/
-# 18. WezTerm           (terminal emulator)      https://wezfurlong.org/wezterm/
-# 19. Whiskey           (Wine Wrapper for Mac)   https://github.com/Whisky-App/Whisky
-brew install --cask discord gimp gpg-suite keepingyouawake librewolf logitech-g-hub medis pgadmin4 qlcolocode qlmarkdown qlprettypatch qlvideo quicklook-csv quicklook-json quicklookase rapidapi rectangle rocket sequel-ace slack sqlitestudio streamlabs visual-studio-code webquicklook wezterm whiskey
+#  4. Krita             (painting program)       https://krita.org/en/
+#  5. LibreWolf         (Firefox clone)          https://librewolf.net/
+#  6. Logitech G HUB    (light controller)       https://www.logitechg.com/en-us/innovation/g-hub.html
+#  7. Medis             (Modern GUI for Redis)   https://getmedis.com/
+#  8. pgAdmin           (PostgresSQL admin)      https://www.pgadmin.org/
+#  9. PostgresApp       (Postgres App)           https://postgresapp.com/
+# 10. QuickLook Plugins (makes quicklook better) https://www.quicklookplugins.com/
+# 11. RapidAPI          (api client)             https://rapidapi.com
+# 12. Rectangle         (window manager)         https://rectangleapp.com/
+# 13. Rocket            (emoji app)              https://matthewpalmer.net/rocket/
+# 14. Sequel Ace        (database client)        https://sequel-ace.com/
+# 15. Slack             (chat client)            https://slack.com/
+# 16. SQLiteStudio      (sqlite database client) https://sqlitestudio.pl/
+# 17. StreamLabs        (streaming software)     https://streamlabs.com/
+# 18. VSCode            (text editor)            https://code.visualstudio.com/
+# 19. WezTerm           (terminal emulator)      https://wezfurlong.org/wezterm/
+# 20. Whiskey           (Wine Wrapper for Mac)   https://github.com/Whisky-App/Whisky
+brew install --cask discord gpg-suite keepingyouawake krita librewolf logitech-g-hub medis pgadmin4 qlcolocode qlmarkdown qlprettypatch qlvideo quicklook-csv quicklook-json quicklookase rapidapi rectangle rocket sequel-ace slack sqlitestudio streamlabs visual-studio-code webquicklook wezterm whiskey
 
 # https://librewolf.net/docs/faq/#why-is-librewolf-marked-as-broken
 brew install --cask librewolf --no-quarantine
