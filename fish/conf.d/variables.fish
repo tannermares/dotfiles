6b5760502953e21526b6d69f7d83cf56501e1ba3
Tanner Mares <tannermares@gmail.com>
2026-03-23 19:49:12 -0700

    added android vars

--- fish/conf.d/variables.fish
+++ fish/conf.d/variables.fish
@@ -6,4 +6,6 @@ if not set --query --global MY_VAR_SET
   set -gx VISUAL $EDITOR
   set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'" # Enables syntax highlighting in manpages
   set -gx XDG_CONFIG_HOME "$HOME/.config" # Sets config directory for lazygit
+  set -gx ANDROID_HOME $HOME/Library/Android/sdk
+  fish_add_path $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator
 end
